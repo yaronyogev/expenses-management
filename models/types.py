921bca90e8cfbc11bfe995f253b2a57cb3a879aa
Yaron Yogev <yaronyogev@gmail.com>
2014-07-07 22:29:18 +0300

    added members type_id, active; added ExpenseType.get_key  and expense_type_get_by_type_id

--- models/types.py
+++ models/types.py
@@ -2,6 +2,19 @@ from google.appengine.ext import ndb
 
 
 class ExpenseType(ndb.Model):
-    expense_type = ndb.IntegerProperty(required=True)
+    type_id = ndb.IntegerProperty(required=True)
     description = ndb.StringProperty(required=True)
+    active = ndb.BooleanProperty(required=True)
+    
+    def get_key(self, expense_type):
+        # Construct a Datastore key for an expense type entity with it's name
+        return ndb.Key('ExpenseType', expense_type)
+    
+def expense_type_get_by_type_id(account, type_id):
+    for et in ExpenseType.query(ancestor=account).fetch():
+        if et.type_id == int(type_id):
+            return et
+    else:
+        return None
+
     
