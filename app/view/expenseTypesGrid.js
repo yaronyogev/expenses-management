b5946013e0e6685f5aa29523ae7034def5556f7a
Yaron Yogev <yaronyogev@gmail.com>
2014-07-12 22:51:14 +0300

    add expense type deletion & deactivation, use AJAX proxy instead of REST proxy

--- app/view/expenseTypesGrid.js
+++ app/view/expenseTypesGrid.js
@@ -1,3 +1,5 @@
+var types_grid = null;
+
 Ext.define('expenses.view.ExpenseTypesGrid', {
     extend: 'Ext.grid.Panel',
     requires: [
@@ -13,18 +15,17 @@ Ext.define('expenses.view.ExpenseTypesGrid', {
     {
         storeId: 'expense_types_store',
         model: 'expenses.model.ExpenseType',
-        autoSync: true,
-        writer: Ext.create('Ext.data.proxy.Rest', {
-            encode: true,
-            root: 'rows',
-            writeAllFields: true,
-            listeners: {
-                exception: function (proxy, response, operation) {
-                    console.log('response:', response);
-                    console.log('operation:', operation);
-                }
+        proxy:
+        {
+            type: 'ajax',
+            reader: {type: 'json', root: 'rows'},
+            url: '/expense_types',
+            extraParams: {
+               account_name: accounts[0].name,
+               account_owner: accounts[0].owner,
+               account_user: user
             }
-        })
+        },
     },
     columns:
     [
@@ -48,7 +49,16 @@ Ext.define('expenses.view.ExpenseTypesGrid', {
                validText: 'Name of expense type is missing'
             }
         },
-        {dataIndex: 'active', text: 'Active'},
+        {
+            dataIndex: 'active',
+            text: 'Active',
+            xtype: 'checkcolumn',
+            listeners: {
+                checkchange: function (cbcol, row, checked) {
+                    types_grid.on_active_edit(cbcol, row, checked);
+                }
+            }
+        },
         {
             xtype: 'actioncolumn',
             width: 50,
@@ -64,7 +74,10 @@ Ext.define('expenses.view.ExpenseTypesGrid', {
                               url: '/expense_types',
                               params: {
                                   action: 'delete',
-                                  id: rec.get('id')
+                                  id: rec.get('id'),
+                                  account_name: accounts[0].name,
+                                  account_owner: accounts[0].owner,
+                                  account_user: user
                               },
                               success: function (response) {
                                   var resp = Ext.decode(response.responseText);
@@ -95,5 +108,91 @@ Ext.define('expenses.view.ExpenseTypesGrid', {
             }
         })
     ],
-    selModel: {selType: 'cellmodel'}
+    selModel: {selType: 'cellmodel'},
+    
+    listeners:{
+        render: function (grid) {
+            types_grid = grid;
+        },
+        edit: function (editor, e) {
+            this.on_cell_edit(editor, e);
+        }
+    },
+    
+    on_cell_edit: function(cell_editing, e) {
+         /* Make an AJAX request */
+         var id = e.record.get('id');
+         var conn = new Ext.data.Connection();
+         conn.request({
+             url: '/expense_types',
+             params: {
+                 action: id == 0 ? 'add' : 'update',
+                 id: id,
+                 name: e.record.get('name'),
+                 active: e.record.get('active'),
+                 account_name: accounts[0].name,
+                 account_owner: accounts[0].owner,
+                 account_user: user
+
+             },
+             success: function(response) {
+                 var json_response = Ext.JSON.decode(response.responseText);
+                 if (json_response.success)
+                 {
+                     var rec = e.record;
+                     if (id == 0) {
+                         rec.beginEdit();
+                         rec.set('id', json_response.id)
+                         rec.endEdit();
+                     }
+                     rec.commit();
+                 }
+                 else
+                 {
+                     // failure processing request
+                     Ext.Msg.alert(get_json_err_text(json_response));
+                     cell_editing.startEditByPosition({
+                         row: e.row,
+                         column: e.column});
+                 }
+             },
+             failure: handle_request_failure
+         });
+         return true;
+     },
+     on_active_edit: function(cbcol, row, checked) {
+         /* Make an AJAX request */
+         var rec = types_grid.store.getAt(row);
+         var id = rec.get('id');
+         var conn = new Ext.data.Connection();
+         conn.request({
+             url: '/expense_types',
+             params: {
+                 action: 'update',
+                 id: id,
+                 name: rec.get('name'),
+                 active: checked,
+                 account_name: accounts[0].name,
+                 account_owner: accounts[0].owner,
+                 account_user: user
+
+             },
+             success: function(response) {
+                 var json_response = Ext.JSON.decode(response.responseText);
+                 if (json_response.success)
+                     rec.commit();
+                 else
+                 {
+                     // failure processing request
+                     Ext.Msg.alert(get_json_err_text(json_response));
+                     this.getPlugin('cell_edit').startEditByPosition({
+                         row: e.row,
+                         column: cbcol.getIndex()});
+                 }
+             },
+             failure: handle_request_failure
+         });
+         return true;
+     }
+
 });
