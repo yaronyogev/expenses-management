2cd070417e4e0c8cc04bc61318495da2b25cea5d
Yaron Yogev <yaronyogev@gmail.com>
2014-07-12 22:43:18 +0300

    expense_type_get_by_type_id(): accept either int or string type_id

--- models/types.py
+++ models/types.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import ndb
-
+import logging
 
 class ExpenseType(ndb.Model):
     type_id = ndb.IntegerProperty(required=True)
@@ -11,8 +11,10 @@ class ExpenseType(ndb.Model):
         return ndb.Key('ExpenseType', expense_type)
     
 def expense_type_get_by_type_id(account, type_id):
+    if not (type(type_id) is int):
+        type_id = int(type_id)
     for et in ExpenseType.query(ancestor=account).fetch():
-        if et.type_id == int(type_id):
+        if et.type_id == type_id:
             return et
     else:
         return None
