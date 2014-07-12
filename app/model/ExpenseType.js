7df72412634415f920dd0bc0d27b49e94f945523
Yaron Yogev <yaronyogev@gmail.com>
2014-07-12 22:46:16 +0300

    entity_id no longer used

--- app/model/ExpenseType.js
+++ app/model/ExpenseType.js
@@ -2,7 +2,6 @@ Ext.define('expenses.model.ExpenseType', {
      extend: 'Ext.data.Model',
      fields: [
         {name: 'id', type: 'int'},
-        {name: 'entity_id', type: 'int'},
         {name: 'name', type: 'string'},
         {name: 'active', type: 'boolean'}
      ],
