ad1102677054f0cbd482ecb662f5a24594de62a7
Yaron Yogev <yaronyogev@gmail.com>
2014-09-08 10:04:27 +0300

    remove unneeded imports remove unneeded logging

--- controllers/expenseTypesController.py
+++ controllers/expenseTypesController.py
@@ -1,8 +1,4 @@
 import webapp2
-import json
-import logging
-
-from google.appengine.api import users
 
 from controllers.RequestController import RequestController
 from models.ExpenseType import ExpenseType
@@ -13,10 +9,8 @@ class ExpenseTypesController(RequestController):
     
     def get(self):
         self.clsRef = ExpenseType
-        logging.info('in ExpenseTypesController get()')
         super(ExpenseTypesController, self).get()
     
     def post(self):
         self.clsRef = ExpenseType
-        logging.info('in ExpenseTypesController post()')
         super(ExpenseTypesController, self).post()
