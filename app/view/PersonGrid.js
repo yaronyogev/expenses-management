24955104d28d64593cb08bd6d901ac3989f2d509
Yaron Yogev <yaronyogev@gmail.com>
2015-04-11 19:18:48 +0300

    removed unused requires

--- app/view/PersonGrid.js
+++ app/view/PersonGrid.js
@@ -1,15 +1,10 @@
 Ext.define('expenses.view.PersonGrid', {
     extend: 'expenses.view.GeneralTypeGrid',
     requires: [
-          'expenses.store.PersonStore',
-          'Ext.grid.plugin.CellEditing',
-          'Ext.selection.CellModel'
+          'expenses.store.PersonStore'
     ],
-    //id: 'person_grid',
     object_type: 'person',
     xtype: 'persongrid',
     url: '/persons',
-    app: null,
-    rtl: true,
     store: Ext.create('expenses.store.PersonStore')
 });
