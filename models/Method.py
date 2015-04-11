fe5360bf9618ee236fe689a7020cca16708da493
Yaron Yogev <yaronyogev@gmail.com>
2015-04-11 21:15:09 +0300

    fix typo in field name

--- models/Method.py
+++ models/Method.py
@@ -4,7 +4,7 @@ from models.GeneralType import GeneralType
 
 class Method(GeneralType):
     monthly_payment = ndb.IntegerProperty(required=False)
-    payment_deffered_days = ndb.IntegerProperty(required=False)
+    payment_deferred_days = ndb.IntegerProperty(required=False)
 
     def set_values(self, id, name, is_active, req):
         GeneralType.set_values(self, id, name, is_active, req)
@@ -15,9 +15,9 @@ class Method(GeneralType):
         if self.monthly_payment < 0 or self.monthly_payment > 31:
             return 'monthly payment day should be between 0 (none) and 31'
         try:
-            self.payment_deffered_days = req.get('payment_deffered_days')
+            self.payment_deferred_days = req.get('payment_deferred_days')
         except InvalidPropertyError:
-            return 'Illegal value in deffered days'
-        if self.payment_deffered_days < 0:
-            return 'payment deffered days does not accept negative values'
+            return 'Illegal value in deferred days'
+        if self.payment_deferred_days < 0:
+            return 'payment deferred days does not accept negative values'
         return True
