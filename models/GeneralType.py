d9f2ccae407d02fd5a941d233accfac6e38e2123
Yaron Yogev <yaronyogev@gmail.com>
2015-04-11 19:50:24 +0300

    added method set_values()

--- models/GeneralType.py
+++ models/GeneralType.py
@@ -5,7 +5,13 @@ class GeneralType(ndb.Model):
     id = ndb.IntegerProperty(required=True)
     name = ndb.StringProperty(required=True)
     active = ndb.BooleanProperty(required=True)
-    
+
+    def set_values(self, id, name, is_active, req):
+        self.id = new_id
+        self.name = name
+        self.active = is_active
+        return True
+
     def get_key(self, name):
         # Construct a Datastore key for a type instance entity with its name
         return ndb.Key(self.__class__, name)
